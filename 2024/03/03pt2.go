e13df24cd3bd5b8e8f500b35252814346e035d22
Squishmeist <ayelee02@gmail.com>
2024-12-03 09:57:01 +0000

    2024 03pt2

--- 2024/03/03pt2.go
+++ 2024/03/03pt2.go
@@ -5,21 +5,42 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
+// do() = multiply
+// don't() = ignore
+// 48
+
 func Pt2(){
-	file, err := os.Open("./03/03e2.txt")
+	file, err := os.Open("./03/03.txt")
     util.AssertNoError(err)
     defer file.Close()
 
 	score := 0
+	do := true
 
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
         line := scanner.Text()
-		matches := findMul(line)
-		score += mul(matches)
+		segments := strings.Split(line, "do")
+
+		for i, segment := range segments{
+			if(i != 0){
+				do = doPrefix(segment)
+			}
+
+			if(do){
+				matches := findMul(segment)
+				score += mul(matches)
+			}
+		}
+
     }
     util.AssertNoError(scanner.Err())
 	fmt.Println("3pt2:", score)
+}
+
+func doPrefix(s string) bool{
+	return strings.HasPrefix(s, "()")
 }
